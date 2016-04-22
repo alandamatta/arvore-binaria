6d24f5ab9229d22e9df2b97b3c926ffe270529c5
Alan da Matta <alandamatta@live.com>
2016-04-21 23:23:41 -0300

    v1

---
+++ src/br/edu/ia/Arvore.java
@@ -0,0 +1,59 @@
+package br.edu.ia;
+
+public class Arvore<T extends Comparable<T>> {
+	
+	private No<T> raiz;
+	
+	
+	//cria árvore vazia
+	Arvore(){
+		this.raiz = null;
+	}
+	
+	//cria árvore nova (add nó raiz)
+	Arvore(No<T> raiz){
+		this.raiz = raiz;
+	}
+	
+	public No<T> getRaiz() {
+		return raiz;
+	}
+	
+	public No<T> inserir(T valor){
+		No<T> no = new No<>(valor);
+		
+		if(raiz == null){
+			raiz = no;
+			return no;
+		}else{
+			//arvore que estiver vazia deve inserir o nó no local correto
+			return inserir(no, raiz);
+		}
+	}
+
+	
+	//metodo para localizar a posição que deverá inserir o nó --> recursividade
+	private No<T> inserir(No<T> no, No<T> raizDaSubArvore) {
+		if(raizDaSubArvore.compareTo(no.getValor()) >= 0){
+			//menor que raiz, então insere a esquerda dela
+			No<T> esquerda = raizDaSubArvore.getEsquerda();
+			if(esquerda == null){
+				raizDaSubArvore.setEsquerda(no);
+				no.setPai(raizDaSubArvore);
+			}else{
+				return inserir(no, esquerda);//verifica mais à esquerda
+			}
+		}else{
+			//maior que raiz, então insere a direita dela
+			No<T> direita = raizDaSubArvore.getDireita();
+			if(direita == null ){
+				raizDaSubArvore.setDireita(no);
+				no.setPai(raizDaSubArvore);
+			}else{
+				return inserir(no, direita);//verifica mais à direita
+			}
+		}
+		return no; //nó inserido
+	}
+	
+}
---
+++ src/br/edu/ia/No.java
@@ -0,0 +1,67 @@
+package br.edu.ia;
+
+public class No<T extends Comparable<T>> implements Comparable<T> {
+
+	T valor; //valor do nó
+	No<T> esquerda; //nó da esquerda
+	No<T> direita; //nó da direita
+	No<T> pai; //nó pai, assim podemos ver a árvore de baixo p/ cima
+	
+	
+	//Construtor
+	No(T valor){
+		this.valor = valor;
+		esquerda = null;
+		direita = null;
+		pai = null;
+	}
+	
+	No(T valor, No<T> pai){
+		this.valor = valor;
+		direita = null;
+		esquerda = null;
+		this.pai = pai;
+	}
+	
+	//getters e setters
+	
+	
+	
+	@Override
+	public int compareTo(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public T getValor() {
+		return valor;
+	}
+
+	public void setValor(T valor) {
+		this.valor = valor;
+	}
+
+	public No<T> getEsquerda() {
+		return esquerda;
+	}
+
+	public void setEsquerda(No<T> esquerda) {
+		this.esquerda = esquerda;
+	}
+
+	public No<T> getDireita() {
+		return direita;
+	}
+
+	public void setDireita(No<T> direita) {
+		this.direita = direita;
+	}
+
+	public No<T> getPai() {
+		return pai;
+	}
+
+	public void setPai(No<T> pai) {
+		this.pai = pai;
+	}
+}
